192e4f33ebe26abbe953f7595df1f820433dcdb7
Karina Boronina <Gubashina@yandex.ru>
2023-03-10 10:57:06 +0300

    add burger

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const header = document.querySelector('.header');
-
 (function () {
+    const header = document.querySelector('.header');
+
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
             header.classList.add('header_scroll');
@@ -9,4 +9,21 @@ const header = document.querySelector('.header');
             header.classList.remove('header_scroll');
         }
     }
+}());
+
+
+
+(function () {
+    const burger = document.querySelector('.burger');
+    const headerMenu = document.querySelector('.header__menu');
+
+    burger.addEventListener('click', () => {
+        headerMenu.classList.add('header__menu_active');
+    })
+
+    const burgerClose = document.querySelector('.header__burger-close');
+
+    burgerClose.addEventListener('click', () => {
+        headerMenu.classList.remove('header__menu_active');
+    })
 }());
